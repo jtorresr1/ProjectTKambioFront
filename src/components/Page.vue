b17600fca7e17fb5a6d5acc35fc97a8ce6755533
jtorresr1 <jaimetr97@gmail.com>
2022-06-19 19:05:46 -0500

    Create Page vue

---
+++ src/components/Page.vue
@@ -0,0 +1,23 @@
+<template>
+  <headerMain />
+  <BodyMain />
+</template>
+
+<script>
+import headerMain from "@/components/Header/Header";
+import BodyMain from "@/components/Body/Body";
+
+export default {
+  name: 'PageMain',
+  components: {
+    BodyMain,
+    headerMain,
+  },
+
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
